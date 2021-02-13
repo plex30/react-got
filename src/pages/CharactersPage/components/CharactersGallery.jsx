23afc53b6eb653627dfa857bc901a433730c2dc4
plex30 <elplanbee@gmail.com>
2021-02-13 18:23:59 +0100

    add styles

--- src/pages/CharactersPage/components/CharactersGallery.jsx
+++ src/pages/CharactersPage/components/CharactersGallery.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import "./CharactersGallery.scss";
 
 export function CharacterGallery(props){
 
     
     return(
-        <div>
+        <div className="c-characters-gallery">
         <div className="row">
         {props.charactersGallery.map((item, i)=>
         
-        <div key={i} className="col-12 col-sm-6 col-md-4 col-lg-3">
+        <div key={i} className="col-12 col-sm-6 col-md-4 col-lg-2">
         <Link to={"/characters/" + item._id}>
-        <figure >
-            <img src={item.image} alt={item.name}></img>
-            <figcaption>{item.name}</figcaption>
+        <figure>
+            <img className="c-characters-gallery__img" src={item.image} alt={item.name}></img>
+            <figcaption className="c-characters-gallery__name">{item.name}</figcaption>
         </figure>
         </Link>
         </div>
